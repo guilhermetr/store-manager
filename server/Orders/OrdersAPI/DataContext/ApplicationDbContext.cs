2fd22448fc0de006a19423ed1e170cb0350c050c
Guilherme Tarter <guilhermetarter@hotmail.com>
2024-03-25 10:37:05 -0300

    Revert "Common Project"

---
+++ server/Orders/OrdersAPI/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrdersAPI.Service.Models;
+using ProductsAPI.Service.Models;
+
+namespace ProductsAPI.Service.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.OrderItems)
+                .WithOne()
+                .HasForeignKey(oi => oi.OrderId);
+        }
+    }
+}
